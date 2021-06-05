d0b5b56582df80d14a7be71215e3f4d95579f653
jrichardsz <jrichardsz.java@gmail.com>
2021-06-05 17:02:01 -0500

    use this.filename instead caller info with stacktrace

--- index.js
+++ index.js
@@ -7,56 +7,6 @@ var originalRequire = Module.prototype.require;
 var cacheRootPaths = {};
 var cacheModulePaths = {};
 
-module.exports = function(filePath, scope, disableMavenStructure) {
-
-  var callerLocation;
-  if (cacheModulePaths[filePath]) {
-    callerLocation = cacheModulePaths[filePath];
-  } else {
-    callerLocation = getCalleeInfo().location;
-    cacheModulePaths[filePath] = callerLocation;
-  }
-
-  var rootPath;
-  if (cacheRootPaths[callerLocation]) {
-    rootPath = cacheRootPaths[callerLocation]
-  } else {
-    rootPath = getAppRootDirectory(callerLocation)
-    cacheRootPaths[callerLocation] = rootPath;
-  }
-
-  if (typeof disableMavenStructure === 'undefined' || disableMavenStructure === false) {
-    if (scope === 'test') {
-      rootPath += "/src/test";
-    } else {
-      rootPath += "/src/main";
-    }
-  }
-
-  var fullPath = path.join(rootPath, filePath);
-
-  if (!fs.existsSync(fullPath) && !fs.existsSync(fullPath + '.js')) {
-    throw new Error('Could not require requested module at ' + fullPath + ' because does not exist');
-  }
-
-  return require(fullPath);
-};
-
-function getAppRootDirectory(callerLocation) {
-  var f = finder(callerLocation);
-  return path.dirname(f.next().filename);
-}
-
-function getCalleeInfo() {
-  var stack = new Error().stack.split("at ");
-  var functionInfo = "" + stack[4].trim();
-  var fileLocation = functionInfo.substring(functionInfo.indexOf("(") + 1, functionInfo.indexOf(":"));
-  var lineInfo = functionInfo.split(":");
-  return {
-    location: fileLocation,
-    line: lineInfo[1]
-  };
-}
 
 Module.prototype.require = function() {
 
@@ -70,7 +20,7 @@ Module.prototype.require = function() {
     if (cacheModulePaths[arguments['0']]) {
       callerLocation = cacheModulePaths[arguments['0']];
     } else {
-      callerLocation = getCalleeInfo().location;
+      callerLocation = this.filename;
       cacheModulePaths[arguments['0']] = callerLocation;
     }
 
@@ -87,7 +37,6 @@ Module.prototype.require = function() {
     try {
       fullPath = path.join(rootPath, "src", "main", initialPath);
       arguments['0'] = fullPath;
-      // log(arguments);
       return originalRequire.apply(this, arguments);
     } catch (err1) {
       if(err1.code !== 'MODULE_NOT_FOUND'){
@@ -101,8 +50,13 @@ Module.prototype.require = function() {
         if(err2.code !== 'MODULE_NOT_FOUND'){
           throw err2;
         }
-        throw new Error(`${initialPath} was not found as npm module, neither in ${rootPath}/src/main nor in ${rootPath}/src/test`);
+        throw new Error(`${initialPath} was not found as npm module, neither in ${rootPath}/src/main nor in ${rootPath}/src/test. Caller: ${this.filename}`);
       }
     }
   }
 };
+
+function getAppRootDirectory(callerLocation) {
+  var f = finder(callerLocation);
+  return path.dirname(f.next().filename);
+}
